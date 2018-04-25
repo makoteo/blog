9ff6cd07c73c4c9d83b52646edf6a236066903ee
makoteo <makoteovideos@gmail.com>
2018-04-24 18:20:01 -0700

    Fixed small bug in infinite mode...

--- _games/flying-cube-dev/game01.js
+++ _games/flying-cube-dev/game01.js
@@ -232,13 +232,14 @@ function game(){
             pipesX.push(pipes[i].getX());
         }
 
-
-        for (var j = 0; j < pipesX.length - 1; j++) { //TRY AGAIN!!
-            if ((pipesX[j + 1] - pipesX[j] < 400) && (pipesX[j + 1] - pipesX[j] > 0) && (pipesX[j + 1] != pipesX[j])) {
-                pipes[j + 1].move = false;
-                console.log("Slowdown... ");
-            } else {
-                pipes[j + 1].move = true;
+        if(gameMode === "Speed") {
+            for (var j = 0; j < pipesX.length - 1; j++) { //TRY AGAIN!!
+                if ((pipesX[j + 1] - pipesX[j] < 370) && (pipesX[j + 1] - pipesX[j] > 0) && (pipesX[j + 1] != pipesX[j])) {
+                    pipes[j + 1].move = false;
+                    console.log("Slowdown... ");
+                } else {
+                    pipes[j + 1].move = true;
+                }
             }
         }
 
