a5df732c22be3263d28eddf018b01eb6f48265dc
makoteo <makoteovideos@gmail.com>
2018-07-19 13:05:41 +0200

    Testing Load

--- _games/voxel-biome/tmp.js
+++ _games/voxel-biome/tmp.js
@@ -3670,4 +3670,18 @@ function repeatOften() {
     game();
     requestAnimationFrame(repeatOften);
 }
-requestAnimationFrame(repeatOften);
+
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
+
+if(document.getElementById("loader").style.display = "none"){
+    requestAnimationFrame(repeatOften);
+}
