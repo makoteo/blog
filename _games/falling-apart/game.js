642a1341e5b130abfa4be3e5fa689dcbaf758343
makoteo <makoteovideos@gmail.com>
2019-02-05 14:41:26 +0100

    Up down Collision

--- _games/falling-apart/game.js
+++ _games/falling-apart/game.js
@@ -19,6 +19,7 @@ var map = [
 var tileSize;
 
 var tiles = [];
+var players = [];
 
 tileSize = Math.round((HEIGHT - HEIGHT/10) / map.length);
 
@@ -49,6 +50,72 @@ function Tile(x, y, width, height, type){
     }
 }
 
+function Player(x, y, type){
+    this.x = x;
+    this.y = y;
+
+    this.width = 30;
+    this.height = 30;
+
+    this.tilePosX = 0;
+    this.tilePosY = 0;
+
+    this.tilePosXRem = 0;
+    this.tilePosYRem = 0;
+
+    this.speed = 3;
+    this.type = type;
+    this.movingUp = false;
+    this.movingDown = false;
+    this.movingLeft = false;
+    this.movingRight = false;
+    this.update = function(){
+
+        //COLLISION CHECK
+        if(this.tilePosY > 0) {
+            if(map[this.tilePosY - 1][this.tilePosX] === 1){
+                if(this.tilePosYRem < this.height/2){
+                    this.movingUp = false;
+                }
+            }
+        }
+        if(this.tilePosY < map.length) {
+            if(map[this.tilePosY + 1][this.tilePosX] === 1){
+                if(this.tilePosYRem > tileSize - this.height/2){
+                    this.movingDown = false;
+                }
+            }
+        }
+
+        if(this.movingUp){
+            this.y-= this.speed;
+        }
+        if(this.movingDown){
+            this.y+= this.speed;
+        }
+        if(this.movingRight){
+            this.x+= this.speed;
+        }
+        if(this.movingLeft){
+            this.x-= this.speed;
+        }
+
+        this.tilePosX = Math.floor((this.x - this.width - xOffset + tileSize/2)/tileSize);
+        this.tilePosXRem = (this.x - this.width - xOffset + tileSize/2) % tileSize;
+
+        this.tilePosY = Math.floor((this.y - this.height - yOffset + tileSize/2)/tileSize);
+        this.tilePosYRem = (this.y - this.height - yOffset + tileSize/2) % tileSize;
+
+        console.log(this.tilePosX + " - " + this.tilePosXRem);
+        console.log(this.tilePosY + " - " + this.tilePosYRem);
+
+    }
+    this.draw = function() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x - this.width / 2, this.y - this.height / 2, 30, 30);
+    }
+}
+
 //CREATE TILES
 for(var i = 0; i < map.length; i++){
     for(var j = 0; j < map[0].length; j++){
@@ -60,12 +127,40 @@ for(var i = 0; i < map.length; i++){
     }
 }
 
+players.push(new Player(WIDTH/2, HEIGHT/2, 0))
+
 function game(){
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-    for(var i = 0; i < tiles.length;i++){
+    for(var i = 0; i < tiles.length; i++){
         tiles[i].update();
         tiles[i].draw();
+    }
+    for(var i = 0; i < players.length; i++) {
+        //if(players[i].type === 0){
+            if(keys && keys[37]){
+                players[i].movingLeft = true;
+            }else{
+                players[i].movingLeft = false;
+            }
+            if(keys && keys[38]){
+                players[i].movingUp = true;
+            }else{
+                players[i].movingUp = false;
+            }
+            if(keys && keys[39]){
+                players[i].movingRight = true;
+            }else{
+                players[i].movingRight = false;
+            }
+            if(keys && keys[40]){
+                players[i].movingDown = true;
+            }else{
+                players[i].movingDown = false;
+            }
+        //}
+        players[i].draw();
+        players[i].update();
     }
 }
 
@@ -74,3 +169,18 @@ function repeatOften() {
     game();
     requestAnimationFrame(repeatOften);
 }
+
+var keys;
+
+window.addEventListener('keydown', function (e) {
+    keys = (keys || []);
+    keys[e.keyCode] = (e.type == "keydown");
+
+    if([32, 37, 38, 39, 40, 114, 112].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+
+}, false);
+window.addEventListener('keyup', function (e) {
+    keys[e.keyCode] = (e.type == "keydown");
+}, false);
