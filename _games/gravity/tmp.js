ad2424a5c3c48a1c6d3c4565d62f125e99f7b9f3
makoteo <makoteovideos@gmail.com>
2019-03-12 19:27:45 +0100

    Added Zoom

--- _games/gravity/tmp.js
+++ _games/gravity/tmp.js
@@ -10,9 +10,19 @@ var ctx = canvas.getContext("2d");
 
 var cameraZoom = 1;
 var screenHalfWidth = WIDTH/2;
+var screenHalfHeight = HEIGHT/2;
 
 var massMultiplier = 10;
 var G = 10;
+
+var PAUSED = false;
+
+var mousePosX = 0;
+var mousePosY = 0;
+
+var clickTimer = 1;
+
+var dragging = false;
 
 // ---------------------------------------------------------- OBJECTS ------------------------------------------------------------------------ //
 
@@ -22,15 +32,15 @@ function Object(x, y, mass, density, type){
     this.mass = mass * massMultiplier;
     this.density = density;
 
-
     this.velX = 0;
     this.velY = 0;
 
-    this.radius = Math.sqrt(this.mass/(this.density*3.14));
+    this.radius = Math.sqrt(this.mass/(this.density*3.14)) * cameraZoom;
 
     this.type = type;
 
     this.cameraX = ((this.x - screenHalfWidth) * cameraZoom + screenHalfWidth);
+    this.cameraY = ((this.y - screenHalfWidth) * cameraZoom + screenHalfHeight);
 
     this.tempX = 0;
     this.tempY = 0;
@@ -44,11 +54,14 @@ function Object(x, y, mass, density, type){
     if(this.type === 1){
         this.affectedByGravity = false;
     }
+    if(this.type === 2){
+        this.velX = -5;
+    }
 
     this.draw = function(){
         ctx.fillStyle = 'blue';
         ctx.beginPath();
-        ctx.arc(this.cameraX, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.arc(this.cameraX, this.cameraY, this.radius, 0, 2 * Math.PI);
         ctx.fill();
 
     };
@@ -72,20 +85,26 @@ function Object(x, y, mass, density, type){
                             this.distance = 1;
                         }
                         if(objects[j].affectedByGravity === true) {
-                            this.velX = this.velX + (objects[j].velX) * (objects[j].mass / this.mass);
-                            this.velY += (objects[j].velY) * (objects[j].mass / this.mass);
-                            this.x = objects[j].x;
-                            this.y = objects[j].y;
+                            this.velX = this.velX + (objects[j].velX);
+                            this.velY += (objects[j].velY);
+                            if(this.mass <= objects[j].mass){
+                                this.x = objects[j].x;
+                                this.y = objects[j].y;
+                            }
                         }else{
                             this.velX = 0;
                             this.velY = 0;
-                            this.x = objects[j].x;
-                            this.y = objects[j].y;
+                            if(this.mass <= objects[j].mass){
+                                this.x = objects[j].x;
+                                this.y = objects[j].y;
+                            }
+                            this.affectedByGravity = false;
                         }
                         this.mass += objects[j].mass;
                         this.radius = Math.sqrt(this.mass/(this.density*3.14));
                         objects[j].inactive = true;
                         objects.splice(j, 1);
+                        console.log(this.distance);
                         break;
                     }
 
@@ -94,15 +113,20 @@ function Object(x, y, mass, density, type){
                             this.distance = 1;
                         }
                         if(objects[j].affectedByGravity === true){
-                            this.velX = this.velX + (objects[j].velX)*(objects[j].mass/this.mass);
-                            this.velY += (objects[j].velY)*(objects[j].mass/this.mass);
-                            this.x = objects[j].x;
-                            this.y = objects[j].y;
+                            this.velX = this.velX + (objects[j].velX);
+                            this.velY += (objects[j].velY);
+                            if(this.mass <= objects[j].mass){
+                                this.x = objects[j].x;
+                                this.y = objects[j].y;
+                            }
                         }else{
                             this.velX = 0;
                             this.velY = 0;
-                            this.x = objects[j].x;
-                            this.y = objects[j].y;
+                            if(this.mass <= objects[j].mass){
+                                this.x = objects[j].x;
+                                this.y = objects[j].y;
+                            }
+                            this.affectedByGravity = false;
                         }
                         this.mass += objects[j].mass;
                         this.radius = Math.sqrt(this.mass/(this.density*3.14));
@@ -122,6 +146,9 @@ function Object(x, y, mass, density, type){
         this.y += this.velY;
 
         this.cameraX = ((this.x - screenHalfWidth) * cameraZoom + screenHalfWidth);
+        this.cameraY = ((this.y - screenHalfHeight) * cameraZoom + screenHalfHeight);
+        this.radius = Math.sqrt(this.mass/(this.density*3.14)) * cameraZoom;
+
     }
 }
 
@@ -129,25 +156,36 @@ objects.push(new Object(WIDTH/2, HEIGHT/2, 50, 1, 1));
 objects.push(new Object(WIDTH/3, 40, 10, 1, 0));
 
 function game(){
+
+    window.onmousemove = logMouseMove;
+
     //SKY FILL
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    if(PAUSED === false){
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    for(var i = 0; i < objects.length; i++){
-        if(objects[i].inactive === false){
-            objects[i].draw();
-            objects[i].update();
+        for(var i = 0; i < objects.length; i++){
+            if(objects[i].inactive === false){
+                objects[i].draw();
+                objects[i].update();
 
+            }
         }
-    }
-    for(var i = 0; i < objects.length; i++){
-        if(objects[i].inactive === false){
-            if(i < objects.length){
+        for(var i = 0; i < objects.length; i++){
+            if(objects[i].inactive === false){
                 objects[i].move();
             }
         }
     }
 
+    if(clickTimer === 0){
+        objects.push(new Object(mousePosX, mousePosY, 10, 1, 2));
+        console.log(objects.length);
+    }
+
+    if(clickTimer < 1){
+        clickTimer++;
+    }
 
     if(gameRunning === true) {
 
@@ -192,6 +230,29 @@ window.addEventListener('keydown', function (e) {
 window.addEventListener('keyup', function (e) {
     keys[e.keyCode] = (e.type == "keydown");
 }, false);
+
+document.addEventListener("mouseup", clickedNow);
+document.addEventListener("mousedown", draggedNow);
+
+function clickedNow(){
+    if(clickTimer === 1){
+        clickTimer = 0;
+        dragging = false;
+    }
+}
+
+function draggedNow(){
+    dragging = true;
+}
+
+function logMouseMove(e) {
+    e = event || window.event;
+
+    var rect = canvas.getBoundingClientRect();
+
+    mousePosX = e.clientX - rect.left;
+    mousePosY = e.clientY - rect.top;
+}
 
 // ---------------------------------------------------------- RELOAD FUNCTION ------------------------------------------------------------------------ //
 
