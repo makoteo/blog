4e717bed99c57c3d7e57c2a439085d4c7343a961
makoteo <makoteovideos@gmail.com>
2020-07-10 12:01:09 +0200

    Rotating and moving paddles?

--- _games/pong-better-maybe/tmp.js
+++ _games/pong-better-maybe/tmp.js
@@ -13,7 +13,7 @@ var ctx = canvas.getContext("2d");
 
 var COLORS = {bg: "#081518", darkgreen: "#102E2F", lightgreen: "#2A7E63", lightblue: "#B6D3E7", yellow:"#CCAF66", red:"#D28A77", white:"#F9EFEC"};
 var CONTROLS = {a: [87, 83], b: [38, 40], c:[69, 68], d: [100, 97]};
-var FONTSIZES = {large: 70};
+var FONTSIZES = {large1: 70, large2: 70};
 
 var objects = [];
 var projectiles = [];
@@ -24,34 +24,72 @@ var maxProjectiles = 2;
 
 // ---------------------------------------------------------- OBJECTS ------------------------------------------------------------------------ //
 
-function Object(x, y, width, height, controls){
+function Object(x, y, width, height, controls, type, bounds){
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.length = this.height;
     this.controls = controls;
+    this.type = type;
 
     this.angle = Math.PI/2;
+    this.omega = 0;
+    this.maxRot = 0.04;
+    this.angleAccel = 0.002;
 
     this.speed = 8;
 
-    this.boundsY = [0, HEIGHT];
+    this.boundsY = bounds;
+
+    this.ctrlReleased = [true, true];
+
+    this.dividerSize = 10;
 
     this.update = function(){
         if(this.controls.length > 0) {
-            if (keys && keys[controls[0]]) {
-                if(this.y > this.boundsY[0]){
-                    this.y -= this.speed;
+            if(this.type === 0){
+                if (keys && keys[controls[0]]) {
+                    if(this.y > this.boundsY[0]){
+                        this.y -= this.speed;
+                    }
                 }
-            }
-            if (keys && keys[controls[1]]) {
-                if(this.y+this.height < this.boundsY[1]){
-                    this.y += this.speed;
+                if (keys && keys[controls[1]]) {
+                    if(this.y+this.height < this.boundsY[1]){
+                        this.y += this.speed;
+                    }
+                }
+            }else if(this.type === 1){
+                if (keys && keys[controls[0]] && this.ctrlReleased[0] === true) {
+                    if(this.y > this.boundsY[0]){
+                        this.y -= this.height+this.dividerSize;
+                        this.ctrlReleased[0] = false;
+                    }
+                }
+                if (keys && keys[controls[1]] && this.ctrlReleased[1] === true) {
+                    if(this.y+this.height < this.boundsY[1]){
+                        this.y += this.height+this.dividerSize;
+                        this.ctrlReleased[1] = false;
+                    }
+                }
+
+                if (keys && !keys[controls[0]]) {
+                    this.ctrlReleased[0] = true;
+                }
+                if (keys && !keys[controls[1]]) {
+                    this.ctrlReleased[1] = true;
+                }
+            }else if(this.type === 2){
+                if (keys && keys[controls[0]] && this.omega < this.maxRot) {
+                    this.omega+=this.angleAccel;
+                }
+                if (keys && keys[controls[1]] && this.omega > -this.maxRot) {
+                    this.omega-=this.angleAccel;
                 }
             }
         }
-        //this.angle+=0.08;
+        this.angle+=this.omega;
+        this.omega*=0.95;
     };
 
     this.draw = function(){
@@ -62,12 +100,23 @@ function Object(x, y, width, height, controls){
         ctx.fillRect(-this.width/2, 0, this.width, this.height);
         ctx.restore();
 
-        /*ctx.strokeStyle = COLORS.red;
+        if(this.type === 1){
+            ctx.strokeStyle = COLORS.yellow;
+            ctx.globalAlpha = 0.5;
+            ctx.setLineDash([this.width/2, this.width/2]);
+            ctx.strokeRect(this.x-this.width/2, this.boundsY[0], this.width, this.height);
+            ctx.strokeRect(this.x-this.width/2, this.boundsY[0] + this.height + this.dividerSize, this.width, this.height);
+            ctx.strokeRect(this.x-this.width/2, this.boundsY[0] + 2*this.height + 2*this.dividerSize, this.width, this.height);
+            ctx.globalAlpha = 1;
+            ctx.setLineDash([0, 0]);
+        }
+
+        ctx.strokeStyle = COLORS.red;
         ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x + this.length*Math.cos(this.angle), this.y + this.length*Math.sin(this.angle));
+        ctx.moveTo(this.x + this.width/2*Math.cos(this.angle+Math.PI/2), this.y + this.width/2*Math.sin(this.angle+Math.PI/2));
+        ctx.lineTo(this.x + this.width/2*Math.cos(this.angle+Math.PI/2) + this.length*Math.cos(this.angle), this.y + this.width/2*Math.sin(this.angle+Math.PI/2) + this.length*Math.sin(this.angle));
         ctx.stroke();
-        ctx.strokeStyle = COLORS.white;*/
+        ctx.strokeStyle = COLORS.white;
     };
 }
 
@@ -94,7 +143,7 @@ function Projectile(x, y, angle){
         }
         //BOUNCES OFF PADDLES
         for(var o in objects){
-            var t = intersect([{x: this.x, y: this.y}, {x: this.x+this.velX*1.1, y: this.y+this.velY*1.1}],[{x: objects[o].x, y: objects[o].y}, {x: objects[o].x + objects[o].length*Math.cos(objects[o].angle), y: objects[o].y + objects[o].length*Math.sin(objects[o].angle)}]);
+            /*var t = intersect([{x: this.x, y: this.y}, {x: this.x+this.velX*1.2, y: this.y+this.velY*1.2}],[{x: objects[o].x + objects[o].width/2*Math.cos(objects[o].angle+Math.PI/2), y: objects[o].y + objects[o].width/2*Math.sin(objects[o].angle+Math.PI/2)}, {x: objects[o].x + objects[o].length*Math.cos(objects[o].angle) + objects[o].width/2*Math.cos(objects[o].angle-Math.PI/2), y: objects[o].y + objects[o].width/2*Math.sin(objects[o].angle-Math.PI/2) + objects[o].length*Math.sin(objects[o].angle)}]);
             if(t === 'collinear') {continue;}
             if(t[0] <= 1 && t[0] >= 0 && t[1] <= 1 && t[1] >= 0) {
                 var rndOff = 0;
@@ -104,6 +153,45 @@ function Projectile(x, y, angle){
                 this.angle = (Math.PI - this.angle) + (objects[o].angle-Math.PI/2)*2 - rndOff*Math.sign(this.velX)+ (Math.random() * (0.4) - 0.2); // + (Math.random() * (1) - 0.5)
                 this.velX = this.speed*Math.cos(this.angle);
                 this.velY = this.speed*Math.sin(this.angle);
+                objects[o].omega*=0.8;
+            }else{
+                t = intersect([{x: this.x, y: this.y}, {x: this.x+this.velX*1.2, y: this.y+this.velY*1.2}],[{x: objects[o].x + objects[o].width/2*Math.cos(objects[o].angle+Math.PI/2), y: objects[o].y + objects[o].width/2*Math.sin(objects[o].angle+Math.PI/2)}, {x: objects[o].x + objects[o].length*Math.cos(objects[o].angle) + objects[o].width/2*Math.cos(objects[o].angle-Math.PI/2), y: objects[o].y + objects[o].width/2*Math.sin(objects[o].angle-Math.PI/2) + objects[o].length*Math.sin(objects[o].angle)}]);
+                if(t === 'collinear') {continue;}
+                if(t[0] <= 1 && t[0] >= 0 && t[1] <= 1 && t[1] >= 0) {
+                    var rndOff = 0;
+                    if(this.type === 0){
+                        rndOff = (this.y - (objects[o].y + objects[o].height/2))/100;
+                    }
+                    this.angle = (Math.PI - this.angle) + (objects[o].angle-Math.PI/2)*2 - rndOff*Math.sign(this.velX)+ (Math.random() * (0.4) - 0.2); // + (Math.random() * (1) - 0.5)
+                    this.velX = this.speed*Math.cos(this.angle);
+                    this.velY = this.speed*Math.sin(this.angle);
+                    objects[o].omega*=0.8;
+                }
+            }*/
+            var t = intersect([{x: this.x, y: this.y}, {x: this.x+this.velX*1.2, y: this.y+this.velY*1.2}],[{x: objects[o].x, y: objects[o].y}, {x: objects[o].x + objects[o].length*Math.cos(objects[o].angle), y: objects[o].y + objects[o].length*Math.sin(objects[o].angle)}]);
+            if(t === 'collinear') {continue;}
+            if(t[0] <= 1 && t[0] >= 0 && t[1] <= 1 && t[1] >= 0) {
+                var rndOff = 0;
+                if(this.type === 0){
+                    rndOff = (this.y - (objects[o].y + objects[o].height/2))/100;
+                }
+                this.angle = (Math.PI - this.angle) + (objects[o].angle-Math.PI/2)*2 - rndOff*Math.sign(this.velX)+ (Math.random() * (0.4) - 0.2); // + (Math.random() * (1) - 0.5)
+                this.velX = this.speed*Math.cos(this.angle);
+                this.velY = this.speed*Math.sin(this.angle);
+                objects[o].omega*=0.8;
+            }else if(this.type === 2){
+                t = intersect([{x: this.x, y: this.y}, {x: this.x+this.velX*1.2, y: this.y+this.velY*1.2}],[{x: objects[o].x, y: objects[o].y}, {x: objects[o].x + objects[o].length*Math.cos(objects[o].angle+objects[o].omega/2), y: objects[o].y + objects[o].length*Math.sin(objects[o].angle+objects[o].omega/2)}]);
+                if(t === 'collinear') {continue;}
+                if(t[0] <= 1 && t[0] >= 0 && t[1] <= 1 && t[1] >= 0) {
+                    var rndOff = 0;
+                    if(this.type === 0){
+                        rndOff = (this.y - (objects[o].y + objects[o].height/2))/100;
+                    }
+                    this.angle = (Math.PI - this.angle) + (objects[o].angle-Math.PI/2)*2 - rndOff*Math.sign(this.velX)+ (Math.random() * (0.4) - 0.2); // + (Math.random() * (1) - 0.5)
+                    this.velX = this.speed*Math.cos(this.angle);
+                    this.velY = this.speed*Math.sin(this.angle);
+                    objects[o].omega*=0.8;
+                }
             }
         }
         //UPDATE POSITION
@@ -116,6 +204,10 @@ function Projectile(x, y, angle){
         ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
         ctx.fillStyle = COLORS.white;
         ctx.fill();
+        /*ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x +this.velX*12, this.y +this.velY*12);
+        ctx.stroke();*/
     };
 }
 
@@ -129,11 +221,11 @@ function Player(id){
 players.push(new Player(0));
 players.push(new Player(1));
 
-objects.push(new Object(10, HEIGHT/2-50, 10, 100, CONTROLS.a));
-objects.push(new Object(WIDTH-10, HEIGHT/2-50, 10, 100, CONTROLS.b));
+objects.push(new Object(10, HEIGHT/2-50, 10, 100, CONTROLS.a, 0, [0, HEIGHT]));
+objects.push(new Object(WIDTH-10, HEIGHT/2-50, 10, 100, CONTROLS.b, 0, [0, HEIGHT]));
 
-objects.push(new Object(200, HEIGHT/2-50, 10, 100, CONTROLS.c));
-objects.push(new Object(WIDTH-200, HEIGHT/2-50, 10, 100, CONTROLS.d));
+objects.push(new Object(200, HEIGHT/2-50, 10, 100, CONTROLS.c, 2, [HEIGHT/2-50-100-10, HEIGHT/2-50+100+10]));
+objects.push(new Object(WIDTH-200, HEIGHT/2-50, 10, 100, CONTROLS.d, 1, [HEIGHT/2-50-100-10, HEIGHT/2-50+100+10]));
 
 projectiles.push(new Projectile(WIDTH/2, HEIGHT/2, 0));
 projectiles.push(new Projectile(WIDTH/2, HEIGHT/2, Math.PI));
@@ -182,12 +274,14 @@ function game(){
 
             if( projectiles[i].x < 0){
                 players[1].points++;
+                FONTSIZES.large2 = 90;
                 projectiles.splice(i, 1);
                 if(players[1].points % 10 === 0){
                     maxProjectiles++;
                 }
             }else if( projectiles[i].x > WIDTH){
                 players[0].points++;
+                FONTSIZES.large1 = 90;
                 projectiles.splice(i, 1);
                 if(players[0].points % 10 === 0){
                     maxProjectiles++;
@@ -204,11 +298,21 @@ function game(){
             }
         }
 
-        ctx.font = FONTSIZES.large + 'px quickPixel';
+        //TIMERS AND OTHER STUFF
+
+        if(FONTSIZES.large1 > 70){
+            FONTSIZES.large1-=2;
+        }
+        if(FONTSIZES.large2 > 70){
+            FONTSIZES.large2-=2;
+        }
+
         ctx.fillStyle = COLORS.lightblue;
         ctx.textAlign = 'left';
+        ctx.font = FONTSIZES.large1 + 'px quickPixel';
         ctx.fillText(players[0].points, 10, 40);
         ctx.textAlign = 'right';
+        ctx.font = FONTSIZES.large2 + 'px quickPixel';
         ctx.fillText(players[1].points, WIDTH - 10, 40);
 
     }
