33b51c97705120b7fabd39add3990398d05cd110
makoteo <makoteovideos@gmail.com>
2021-04-30 18:02:20 +0200

    Basic stuff working

--- _games/ultimate-tic-tac-toe/tmp.js
+++ _games/ultimate-tic-tac-toe/tmp.js
@@ -6,25 +6,33 @@ var COLORS = {white: "rgb(254, 250, 236)", black: "rgb(7, 5, 14)", blue: "rgb(36
 
 var boards = [
 
-    [1, 1, 1, 1, 0, 1, 0, 0, 1],
-    [1, 1, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 ];
 
 var mainBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+var mousePosX = 0;
+var mousePosY = 0;
+var clicked = false;
 
 // EXAMPLE ARRAY coins = [];
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+var currentTurn = 1;
+var currentBoard = -1;
+
+var gameRunning = true;
 
 // ---------------------------------------------------------- OBJECTS ------------------------------------------------------------------------ //
 
@@ -36,7 +44,19 @@ var ctx = canvas.getContext("2d");
 
 // ---------------------------------------------------------- FUNCTIONS ------------------------------------------------------------------------ //
 
-
+function checkWinCondition(map) {
+    var a = 1;
+    if (map[0] + map[1] + map[2] === a * 3 || map[3] + map[4] + map[5] === a * 3 || map[6] + map[7] + map[8] === a * 3 || map[0] + map[3] + map[6] === a * 3 || map[1] + map[4] + map[7] === a * 3 ||
+        map[2] + map[5] + map[8] === a * 3 || map[0] + map[4] + map[8] === a * 3 || map[2] + map[4] + map[6] === a * 3) {
+        return a;
+    }
+    a = -1;
+    if (map[0] + map[1] + map[2] === a * 3 || map[3] + map[4] + map[5] === a * 3 || map[6] + map[7] + map[8] === a * 3 || map[0] + map[3] + map[6] === a * 3 || map[1] + map[4] + map[7] === a * 3 ||
+        map[2] + map[5] + map[8] === a * 3 || map[0] + map[4] + map[8] === a * 3 || map[2] + map[4] + map[6] === a * 3) {
+        return a;
+    }
+    return 0;
+}
 
 // ---------------------------------------------------------- GAME FUNCTION ------------------------------------------------------------------------ //
 
@@ -98,9 +118,16 @@ function game(){
     }
 
     //Shapes
+    ctx.lineWidth = 5;
     for(var i in boards){
+        if(mainBoard[i] === 0) {
+            if (checkWinCondition(boards[i]) !== 0) {
+                mainBoard[i] = checkWinCondition(boards[i]);
+            }
+        }
         for(var j in boards[i]){
             if(boards[i][j] === 1){
+                ctx.strokeStyle = COLORS.red;
                 ctx.beginPath();
                 ctx.moveTo((WIDTH/3-squareSize)/2 + squareSize/6 - shapeSize + (j%3)*squareSize/3 + (i%3)*WIDTH/3, (WIDTH/3 - squareSize)/2 + squareSize/6 - shapeSize + Math.floor(j/3)*squareSize/3 + Math.floor(i/3)*WIDTH/3);
                 ctx.lineTo((WIDTH/3-squareSize)/2 + squareSize/6 + shapeSize + (j%3)*squareSize/3 + (i%3)*WIDTH/3, (WIDTH/3 - squareSize)/2 + squareSize/6 + shapeSize + Math.floor(j/3)*squareSize/3 + Math.floor(i/3)*WIDTH/3);
@@ -111,45 +138,77 @@ function game(){
                 ctx.lineTo((WIDTH/3-squareSize)/2 + squareSize/6 + shapeSize + (j%3)*squareSize/3 + (i%3)*WIDTH/3, (WIDTH/3 - squareSize)/2 + squareSize/6 - shapeSize + Math.floor(j/3)*squareSize/3 + Math.floor(i/3)*WIDTH/3);
                 ctx.stroke();
             }else if(boards[i][j] === -1){
-                //ctx.beginPath();
-                //ctx.ellipse((WIDTH/3-squareSize)/2 + squareSize/6 + (j%3)*squareSize/3 + (i%3)*WIDTH/3, (WIDTH/3 - squareSize)/2 + squareSize/6 + Math.floor(j/3)*squareSize/3 + Math.floor(i/3)*WIDTH/3);
-                //ctx.stroke();
+                ctx.strokeStyle = COLORS.blue;
+                ctx.beginPath();
+                ctx.ellipse((WIDTH/3-squareSize)/2 + squareSize/6 + (j%3)*squareSize/3 + (i%3)*WIDTH/3, (WIDTH/3 - squareSize)/2 + squareSize/6 + Math.floor(j/3)*squareSize/3 + Math.floor(i/3)*WIDTH/3, shapeSize*1.1, shapeSize*1.1, 0, 0, Math.PI*2);
+                ctx.stroke();
             }
         }
     }
+
+    shapeSize = squareSize/3;
+    ctx.lineWidth = 20;
+
+    for(var j in mainBoard){
+        if(mainBoard[j] === 1){
+            ctx.strokeStyle = COLORS.red;
+            ctx.beginPath();
+            ctx.moveTo(WIDTH/6 - shapeSize + (j%3)*WIDTH/3, WIDTH/6 - shapeSize + Math.floor(j/3)*WIDTH/3);
+            ctx.lineTo(WIDTH/6 + shapeSize + (j%3)*WIDTH/3, WIDTH/6 + shapeSize + Math.floor(j/3)*WIDTH/3);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(WIDTH/6 - shapeSize + (j%3)*WIDTH/3, WIDTH/6 + shapeSize + Math.floor(j/3)*WIDTH/3);
+            ctx.lineTo(WIDTH/6 + shapeSize + (j%3)*WIDTH/3, WIDTH/6 - shapeSize + Math.floor(j/3)*WIDTH/3);
+            ctx.stroke();
+        }else if(mainBoard[j] === -1){
+            ctx.strokeStyle = COLORS.blue;
+            ctx.beginPath();
+            ctx.ellipse(WIDTH/6 + (j%3)*WIDTH/3, WIDTH/6 + Math.floor(j/3)*WIDTH/3, shapeSize*1.1, shapeSize*1.1, 0, 0, Math.PI*2);
+            ctx.stroke();
+        }
+    }
+
+    shapeSize = squareSize/6;
+
+    //mouseClickHandler
+    if(clicked === true) {
+        for (var i in boards) {
+            for (var j in boards[i]) {
+                if(boards[i][j] === 0) {
+                    if (mousePosX > (WIDTH / 3 - squareSize) / 2 + squareSize / 6 - shapeSize + (j % 3) * squareSize / 3 + (i % 3) * WIDTH / 3 && mousePosX < (WIDTH / 3 - squareSize) / 2 + squareSize / 6 + shapeSize + (j % 3) * squareSize / 3 + (i % 3) * WIDTH / 3) {
+                        if (mousePosY > (WIDTH / 3 - squareSize) / 2 + squareSize / 6 - shapeSize + Math.floor(j / 3) * squareSize / 3 + Math.floor(i / 3) * WIDTH / 3 && mousePosY < (WIDTH / 3 - squareSize) / 2 + squareSize / 6 + shapeSize + Math.floor(j / 3) * squareSize / 3 + Math.floor(i / 3) * WIDTH / 3) {
+                            boards[i][j] = currentTurn;
+                            currentTurn = -currentTurn;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    clicked = false;
+
 }
 
 // ---------------------------------------------------------- RESET FUNCTION ------------------------------------------------------------------------ //
 
-function Start(){
-    if(gameRunning === false){
-        SCORE = 0;
-        frameCount = 0;
-
-        /* RESET ARRAYS (EXAMPLE)
-        coins = [];
-        */
-
-        //document.getElementById("score").innerHTML = "" + SCORE;
-        //document.getElementById("gamescore").innerHTML = "" + GAMESCORE;
-        //document.getElementById("scorediv").removeAttribute("hidden");
-        //document.getElementById("gamescorediv").removeAttribute("hidden");
-        HIGHSCORE = localStorage.getItem("HighScoreBusiness");
-        gameRunning = true;
-    }
-}
-
-/* EXAMPLE DIV HIDE
-function ShowInstructions(){
-    document.getElementById("startMenu").setAttribute("hidden", "hidden");
-    document.getElementById("resetMenu").setAttribute("hidden", "hidden");
-    document.getElementById("instructionsMenu").removeAttribute("hidden");
-}
-*/
-
 var keys;
 
 // ---------------------------------------------------------- KEY LISTENERS ------------------------------------------------------------------------ //
+
+function findScreenCoords(mouseEvent)
+{
+    var rect = canvas.getBoundingClientRect();
+    mousePosX = mouseEvent.clientX - rect.left;
+    mousePosY = mouseEvent.clientY - rect.top;
+}
+
+function click(){
+    clicked = true;
+}
+document.getElementById("myCanvas").onmousemove = findScreenCoords;
+document.getElementById("myCanvas").onclick = click;
 
 window.addEventListener('keydown', function (e) {
     keys = (keys || []);
